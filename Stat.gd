401e088140dbde3f24a1a49f8ef9e4ca4c7360e6
Nicola Baribeau <nicolabaribeau@gmail.com>
2024-12-04 13:20:29 -0500

    Fixing bad rename

--- Stat.gd
+++ Stat.gd
@@ -3,5 +3,5 @@ class_name Stat extends Pipe
 func add(coupling: Pipe) -> bool:
 	if get_children().has(coupling) or not coupling is StatModifier:
 		return false
-	add_child(StatModifier.deserialize(StatModifier.serialize(coupling)))
+	add_child(StatModifier.Deserialize(StatModifier.Serialize(coupling)))
 	return true
