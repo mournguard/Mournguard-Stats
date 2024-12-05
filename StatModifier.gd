a32bccfa4708d8129178d298f8661c73456c4a67
Nicola Baribeau <nicolabaribeau@gmail.com>
2024-12-04 19:13:37 -0500

    Changes after making it work on Gyras 0.0.4

--- StatModifier.gd
+++ StatModifier.gd
@@ -1,12 +1,18 @@
 class_name StatModifier extends Pipe
 
-var source: String
 var type: String
 
-func _init(_base: float, _type: String, _sourcename: String) -> void:
+func _init(_type: String, _base: float = 0, _sourcename: String = "") -> void:
 	super(_base)
 	type = _type
-	source = _sourcename if _sourcename else source
+	if _sourcename: name = _sourcename
+
+func add(coupling: Pipe) -> bool:
+	if not coupling is StatModifier or get_children().has(coupling): return false
+	if coupling.is_inside_tree(): coupling = StatModifier.new(coupling.type, coupling.base, coupling.name)
+	add_child(coupling)
+	_dirty = true
+	return true
 
 static func Modifiers() -> Dictionary[String, Variant]:
 	return {}
@@ -15,4 +21,4 @@ static func Serialize(mod: StatModifier) -> Dictionary:
 	return {"type": mod.type, "base": mod.base}
 
 static func Deserialize(data: Dictionary) -> StatModifier:
-	return Modifiers()[data.type].new(data.type, data.add)
+	return Modifiers()[data.type].new(data.type, data.base)
